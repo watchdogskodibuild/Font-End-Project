fc61a58416feadf59de7222d94ef1ce337c7bc09
watchdogskodibuild <elayf00@gmail.com>
2025-05-08 14:38:23 +0300

    ugduhfw

--- DOCS/מטלת בית 4/easy2write/src/components/contact/contact.jsx
+++
@@ -1,5 +0,0 @@
-import styles from '@/styles/text.module.css';
-
-export function Contact() {
-  return <h1 className={styles.title}>Contact us today!</h1>;
-} 
--- DOCS/מטלת בית 4/easy2write/src/components/not-found/not-found.jsx
+++
@@ -1,5 +0,0 @@
-import styles from '@/styles/text.module.css';
-
-export function NoMatch() {
-  return <h1 className={styles.title}>404 - Not Found</h1>;
-} 
